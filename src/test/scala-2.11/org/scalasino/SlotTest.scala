99a9a3b7669cdf8c6851418ee5ba28f77089dd20
kharole <oleg.kharchenko@gmail.com>
2015-11-23 18:26:04 +0200

    use TestFSMRef

--- src/test/scala-2.11/org/scalasino/SlotTest.scala
+++ src/test/scala-2.11/org/scalasino/SlotTest.scala
@@ -2,8 +2,8 @@ package org.scalasino
 
 
 import akka.actor.{Props, ActorSystem}
-import akka.testkit.{ImplicitSender, TestKit}
-import org.scalasino.model.{Spin, SpinOutcome}
+import akka.testkit.{TestFSMRef, ImplicitSender, TestKit}
+import org.scalasino.model.{PickAndClickAwaiting, SpinAwaiting, Spin, SpinOutcome}
 import org.scalatest.{BeforeAndAfterAll, Matchers, WordSpecLike}
 
 class SlotTest extends TestKit(ActorSystem("SlotTest"))
@@ -13,25 +13,24 @@ with BeforeAndAfterAll
 with ImplicitSender {
 
   val r = new RandomNumberGenerator(Vector())
-  val slot = system.actorOf(Props(new Slot("testSlot", r)))
+
+  val slot = TestFSMRef(new Slot("testSlot", r))
 
   override def afterAll: Unit = system.terminate()
 
   "Slot Actor" should {
     "win 1x" in {
-      r.setMock(Vector(2,2,2))
+      r.setMock(Vector(2, 2, 2))
       slot ! Spin(2.00)
       expectMsg(SpinOutcome(2.00, 2.00, 3, 3, 3, false))
-      /*      within(10 millis) {
-              slot ! GetBalance
-              expectMsg(money(10, "EUR"))
-            }*/
+      assert(slot.stateName == SpinAwaiting)
     }
 
     "get pick and click qualified" in {
-      r.setMock(Vector(6,6,6))
+      r.setMock(Vector(6, 6, 6))
       slot ! Spin(2.00)
       expectMsg(SpinOutcome(2.00, 4.00, 7, 7, 7, true))
+      assert(slot.stateName == PickAndClickAwaiting)
     }
 
   }
